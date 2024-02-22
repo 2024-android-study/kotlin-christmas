48be301300876bfb97ad69615f725d3c414b3dbb
Nahyun Kim <hedghog1004@naver.com>
2024-02-23 08:45:53 +0900

    feat: 평일 할인 메서드 구현

---
+++ src/main/kotlin/christmas/constant/DiscountConstant.kt
@@ -0,0 +1,9 @@
+package christmas.constant
+
+enum class DiscountConstant(val value: Int) {
+    DISCOUNT_CHRESTMAS_DDAY(100),
+    DISCOUNT_WEEKDAY(2023),
+    DISCOUNT_WEEKEND(2023),
+    DISCOUNT_SPECIAL(1000),
+    PRESENT_EVENT(120000)
+}
---
+++ src/main/kotlin/christmas/service/EventDiscount.kt
@@ -0,0 +1,33 @@
+package christmas.service
+
+import christmas.config.MenuType
+import christmas.config.OrderedMenu
+import christmas.constant.DiscountConstant
+import java.time.LocalDate
+
+class EventDiscount(val day: Int) {
+    // 평일 할인
+    fun weekdayDessertDiscount(menus: List<OrderedMenu>): Int {
+        if (checkIsWeekend(day)) {
+            var discount = 0
+            for (menu in menus) {
+                if (menu.menu.type == MenuType.APPETIZER) {
+                    discount += DiscountConstant.DISCOUNT_WEEKDAY.value
+                }
+            }
+            return discount
+        }
+        return 0
+    }
+
+    fun checkIsWeekend(day: Int): Boolean {
+        val date = LocalDate.of(2023, 12, day);
+        println(date);  // 2023-12-25
+        val dayOfWeek = date.getDayOfWeek()
+        val dayOfWeekNumber = dayOfWeek.getValue()
+        return when (dayOfWeekNumber) {
+            6, 7 -> true // 주말
+            else -> false // 주중
+        }
+    }
+}
